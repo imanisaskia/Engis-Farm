7f706803b9811dfa90a911c560b4c0f20b792b69
Saskia Imani <imani.saskia@gmail.com>
2019-04-21 18:01:37 +0700

    Added javadoc comments. Also fixed a few inheritance errors.

--- src/animals/Chicken.java
+++ src/animals/Chicken.java
@@ -1,5 +1,8 @@
 package animals;
 
+/** Class of chicken, inherits EggProducingAnimal. Methods include getters for animal noise and hunger rate.
+ * @author: Saskia Imani (13517142)
+ */
 public class Chicken extends EggProducingAnimal {
     private static final String noise = "Bok bok bok!";
     private static final double hungerRate = 1;
